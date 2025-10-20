484d16e651a1d75d17b70cf3ec9c1ce8cb16b714
Ponzing Zingman <zingmanp@gmail.com>
2025-10-20 15:09:55 +0100

    first commit 2

---
+++ src/main/java/com/nimc/nimc_batch_a/NimcBatchAApplication.java
@@ -0,0 +1,13 @@
+package com.nimc.nimc_batch_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NimcBatchAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NimcBatchAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nimc/nimc_batch_a/NimcBatchAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nimc.nimc_batch_a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NimcBatchAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
